8dfd3047b3274b595c978cc8e26908ca64c51bbe
Rafael Cardoso <rafzk@users.noreply.github.com>
2018-05-18 16:24:14 +0100

    Updated

--- Program.cs
+++ Program.cs
@@ -16,18 +16,13 @@ namespace SNOWRESTful4DMPM
     {
         static NameValueCollection AppSettings = ConfigurationManager.AppSettings;
 
-
-
         static void Main(string[] args)
         {
             var settings = new Endpoint();
-
             List<Request> itemsList = new List<Request>();
             itemsList = Utils.ReadSchema(settings.SchemaFile);
             Utils.log.Info($"Found a total of {itemsList.Count} nodes");
-
             ProcessData(itemsList, settings);
-
 
         }
 
@@ -37,30 +32,30 @@ namespace SNOWRESTful4DMPM
 
             for (int j = 0; j < itemsList.Count; j++)
             {
-                Utils.log.Info("Begin transaction #" + j);
+                Utils.log.Info($"Begin transaction #{j}");
                 string apitable = itemsList[j].ApiTable;
                 string fields = itemsList[j].ApiFieldsList;
                 string query = itemsList[j].Query;
                 string limit = itemsList[j].Limit;
 
-                Utils.log.Info("API Table: " + apitable);
-                Utils.log.Debug("API Fields: " + fields);
-                Utils.log.Debug("Query: " + query);
-                Utils.log.Debug("Limit: " + limit);
+                Utils.log.Info($"API Table: {apitable}");
+                Utils.log.Debug($"API Fields: {fields}");
+                Utils.log.Debug($"Query: {query}");
+                Utils.log.Debug($"Limit: {limit}");
 
                 string endpoint = settings.EndPoint;
                 string parameters = settings.RestParameters;
                 endpoint += apitable + "?sysparm_query=" + query + "&sysparm_limit=" + limit + parameters;
-                Utils.log.Debug("Address: " + endpoint);
+                Utils.log.Debug($"Address: {endpoint}");
                 Console.WriteLine(endpoint);
 
 
                 // Call REST
-                Utils.log.Debug("Making the REST call");
+                Utils.log.Debug($"Making the REST call");
 
                 var rest = new API(endpoint, settings.RestContentType, settings.RestUser, settings.RestPwd, settings.RestTimeout);
                 var result = rest.MakeWebRequest();
-                Utils.log.Debug("Result: " + result);
+                Utils.log.Debug($"Result: {result}");
 
                 result = Utils.RemoveSpecialChars(result);
                 dynamic results = JsonConvert.DeserializeObject<dynamic>(result);
@@ -73,8 +68,8 @@ namespace SNOWRESTful4DMPM
                 {
                     string entryResult = string.Empty;
                     string[] splitFields = itemsList[j].ApiFieldsList.Split(',');
-                    Utils.log.Debug("API (#) table affected: " + apitable);
-                    Utils.log.Debug("API (#) fields to collect: " + splitFields.Length);
+                    Utils.log.Debug($"API (#) table affected: {apitable}");
+                    Utils.log.Debug($"API (#) fields to collect: {splitFields.Length}");
                     for (int l = 0; l < splitFields.Length; l++)
                     {
                         //Console.WriteLine(results.result[k][splitFields[l]]);
@@ -85,8 +80,8 @@ namespace SNOWRESTful4DMPM
                         // vai buscar os campos
                         var _key = splitFields[l];
                         var _fields = _key.Split('.');
-                        Utils.log.Debug("Value #" + l + " Parent " + _fields[0]);
-                        Utils.log.Debug("Value #" + l + " Child: " + _fields[1]);
+                        Utils.log.Debug($"Value #{l} Parent {_fields[0]}");
+                        Utils.log.Debug($"Value #{l} Child: {_fields[1]}");
 
                         try
                         {
@@ -98,7 +93,7 @@ namespace SNOWRESTful4DMPM
                         {
                             System.Console.WriteLine(e.Message);
                             // Set IndexOutOfRangeException to the new exception's InnerException.
-                            throw new System.ArgumentOutOfRangeException("index parameter is out of range.", e);
+                            throw new System.ArgumentOutOfRangeException($ "index parameter is out of range. {e}");
                         }
 
                     }
@@ -113,8 +108,8 @@ namespace SNOWRESTful4DMPM
 
                 if (settings.WriteType == "SQL")
                 {
-                    Utils.log.Debug("Method selected: SQL");
-                    Utils.log.Debug("Schema DB Table: " + itemsList[j].DbTable);
+                    Utils.log.Debug($"Method selected: {settings.WriteType}");
+                    Utils.log.Debug($"Schema DB Table: {itemsList[j].DbTable}");
                     using (SqlConnection myConnection = new SqlConnection(settings.ConnectionStringBD))
                     {
                         try
@@ -128,14 +123,14 @@ namespace SNOWRESTful4DMPM
                             {
                                 try
                                 {
-                                    Utils.log.Debug("DB (#) fields to insert: " + resultCollection.Count);
+                                    Utils.log.Debug($"DB (#) fields to insert: {resultCollection.Count}");
                                     for (int m = 0; m < resultCollection.Count; m++)
                                     {
                                         // percorrer cada linha a lista dos campos
                                         string buildQuery = "INSERT INTO " + itemsList[j].DbTable + "(" + itemsList[j].DbFieldsList + ",import_on)";
 
                                         buildQuery += "Values (" + resultCollection[m].ToString() + ",@ImportDate)";
-                                        Utils.log.Debug("SQL query: " + buildQuery);
+                                        Utils.log.Debug($"SQL query: {buildQuery}");
                                         SqlCommand myCommand = new SqlCommand(buildQuery, myConnection, tran);
 
                                         // Para utilizasr o insert sem a transaction
@@ -145,7 +140,7 @@ namespace SNOWRESTful4DMPM
                                         myCommand.ExecuteNonQuery();
                                     }
 
-                                    Utils.log.Info("SQL executed! Total rows affected are " + resultCollection.Count);
+                                    Utils.log.Info($"SQL executed! Total rows affected are {resultCollection.Count}");
                                     tran.Commit();
                                 }
                                 catch
@@ -162,20 +157,20 @@ namespace SNOWRESTful4DMPM
                         }
                         catch (Exception e)
                         {
-                            Utils.log.Error("SQL Transaction Rollback Exception Output: " + e.ToString());
+                            Utils.log.Error($"SQL Transaction Rollback Exception Output: {e.ToString()}");
                             //Console.WriteLine("SQL Transaction Rollback Exception Type: {0}", e.GetType());
-                            Utils.log.Error("SQL Transaction Rollback Exception Type: " + e.GetType());
+                            Utils.log.Error($"SQL Transaction Rollback Exception Type: {e.GetType()}");
                             //Console.WriteLine("Message: {0}", e.Message);
-                            Utils.log.Error("Message: " + e.Message);
+                            Utils.log.Error($"Message: {e.Message}");
                         }
                     }
                 }
                 else if (settings.WriteType == "File")
                 {
-                    Utils.log.Info("Method selected: File");
+                    Utils.log.Info($"Method selected: {settings.WriteType}");
                     Utils.log.Info("Writting data to file");
                     string filePath = AppDomain.CurrentDomain.BaseDirectory;
-                    Utils.log.Info("Path: " + filePath);
+                    Utils.log.Info($"Path: {filePath}");
                     Utils.WriteToFile(filePath, apitable, result);
                 }
                 else
